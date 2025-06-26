3add997456b04e1201fc49bbca9454e319e70dae
Romain Gilliotte <rgilliotte@gmail.com>
2025-06-26 09:03:03 +0200

    refactor: simplify month-week strategies and clean memoize implementation (#10)
    * refactor: clean up month-week-base
    
    * refactor: clean memoize implementation

--- src/timeslot/strategies/month-week-sun.ts
+++ src/timeslot/strategies/month-week-sun.ts
@@ -2,7 +2,6 @@ import { BaseMonthWeekStrategy } from './month-week-base';
 import { TimeSlotPeriodicity } from '../../periodicity';
 
 export class MonthWeekSunStrategy extends BaseMonthWeekStrategy {
-  override readonly weekStartDay: number = 0; // Sunday
   override readonly periodicitySuffix: string = 'sun';
   override readonly periodicity: TimeSlotPeriodicity = TimeSlotPeriodicity.MonthWeekSun;
-} 
+}
